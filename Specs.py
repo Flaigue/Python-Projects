2183d4722787f80c6a5f66292ad5ec3b816b14be
Leandro <leandroreis6282@gmail.com>
2026-01-12 22:15:15 +0000

    Codico Atualizado

--- Specs.py
+++ Specs.py
@@ -17,16 +17,39 @@ print(f"Desktop Environment: {DE}")
 print(f"CodeName Version: {codename.capitalize()}")
 print("CPU Arquitecture:",platform.processor())
 
+count = 0
 modelcpu = ""
 with open("/proc/cpuinfo", "r") as specs_cpu:
     for line in specs_cpu:
         if "model name" in line:
             part_model, part_freq = line.split("@")
             part_model = part_model.replace("model name	:", "").replace("(R)", "").replace("CPU", "")
-            part_model = part_model.split().strip()
+            part_model = part_model.strip().split()
             part_model = " ".join(part_model)
+        if "cache size" in line:
+            if count == 0:
+                cache_cpu = line
+                cache_cpu = cache_cpu.split(":")
+            else:
+                pass
+            count+=1
+        count = 0
+        if "cpu cores" in line:
+            if count == 0:
+                cpu_cores = line
+                cpu_cores = cpu_cores.split(":")
+            else:
+                pass
+            
 print(f"CPU Model: {part_model}")
 print(f"CPU Frequency: {part_freq.strip()}")
+print(f"CPU Cache: {cache_cpu[-1].strip()}")
+print(f"CPU Cores: {cpu_cores[-1].strip()}")
 
+var = str(input(""))
+if var == "":
+    exit()
+else:
+    exit()
 #Colocar Specs CPU, GPU, RAM e Armazenamento como as specs principais dos PC
 #Specs do OS, CPU mais especificamente o modelo da CPU, arquitetura e Frequencia foi concluido
