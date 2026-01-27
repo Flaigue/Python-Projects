318f9ad601e6573ce1f7aa535a78b14f95364deb
Leandro <leandroreis6282@gmail.com>
2026-01-27 19:10:02 +0000

    Atualização do codico, codico finalizado e ainda falta comentarios

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,34 +1,23 @@
-from os import system
+import os
 import random
 import time
 
-aquatic_animals = ["Baleia", "Golfinho", "Tubarao", "Tartaruga", "Polvo", "Lula", "Sardinha", "Caranguejo", "Lagosta", "Cavalo Marinho", "Estrela Do Mar", "Pinguim", "Foca", "Lontra", "Orca", "Arraia", "Camarao", "Medusa", "Bacalhau", "Tubarao Martelo"]
+def limpar():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+aquatic_animals = ["Baleia", "Golfinho", "Tubarao", "Tartaruga", "Polvo", "Lula", "Sardinha", "Caranguejo", "Lagosta", "Cavalo Marinho", "EstrelaDoMar", "Pinguim", "Foca", "Lontra", "Orca", "Arraia", "Camarao", "Medusa", "Bacalhau", "TubaraoMartelo"]
 terrestrial_animals = ["Cachorro", "Gato", "Elefante", "Leao", "Tigre", "Girafa", "Zebra", "Macaco", "Coelho", "Cavalo", "Vaca", "Porco", "Rato", "Esquilo", "Urso", "Raposa", "Canguru", "Cervo", "Lobo", "Hamster"]
-aerial_animals = ["Aguia", "Beija Flor", "Papagaio", "Coruja", "Gaviao", "Pombo", "Andorinha", "Pardal", "Tucano", "Arara", "Pica Pau", "Morcego", "Falcao", "Cisne", "Ganso", "Pato", "Cegonha", "Flamingo", "Peru", "Galinha"]
+aerial_animals = ["Aguia", "BeijaFlor", "Papagaio", "Coruja", "Gaviao", "Pombo", "Andorinha", "Pardal", "Tucano", "Arara", "PicaPau", "Morcego", "Falcao", "Cisne", "Ganso", "Pato", "Cegonha", "Flamingo", "Peru", "Galinha"]
 
 animals_groups = ["Aquático", "Terrestre", "Aéreo"]
-indice_group = random.randint(0, 2)
 categorias_animais  = {
     "Aquático": aquatic_animals, 
     "Terrestre": terrestrial_animals, 
     "Aéreo": aerial_animals
 }
-nomes_categorias = list(categorias_animais.keys())
-tema_sorteado = random.choice(nomes_categorias)
-choisen_animal = random.choice(categorias_animais[tema_sorteado]).upper()
-
-lenght_name = len(choisen_animal)
-hidden_name = [" " if letra == " " else "_" for letra in choisen_animal]
-
-erros = 0
-acertos = 0
-lista_acertos = []
-lista_erros = []
-
-dica_usada = False
 
 stages = [
-    """
+    r"""
 _ _ _  
 |   |  
 |   O  
@@ -37,7 +26,7 @@ _ _ _
 |_  
     """,
 
-    """
+    r"""
 _ _ _
 |   |
 |   O
@@ -46,7 +35,7 @@ _ _ _
 |_
     """,
 
-    """
+    r"""
 _ _ _
 |   |
 |   O
@@ -55,7 +44,7 @@ _ _ _
 |_
     """,
 
-    """
+    r"""
 _ _ _
 |   |
 |   O
@@ -64,7 +53,7 @@ _ _ _
 |_
     """,
 
-    """
+    r"""
 _ _ _ 
 |   |
 |   O
@@ -73,7 +62,7 @@ _ _ _
 |_    
     """,
 
-    """
+    r"""
 _ _ _
 |   |
 |   O
@@ -81,7 +70,7 @@ _ _ _
 |
 |_
     """,
-    """
+    r"""
 _ _ _
 |   |
 |
@@ -92,66 +81,108 @@ _ _ _
 ]
 
 derrota = False
+continuar = True
 
-while "_" in hidden_name:
-    system("clear")
+while continuar == True:
+    derrota = False
+    dica_usada = False
 
-    if erros == 6:
-        system("clear")
-        print(stages[6])
-        derrota = True
-        break
-    print(stages[erros])
-    print(" ".join(hidden_name))
-    print("")
-    palpite = input("Palpite: ").upper()
-    if palpite == "dica":
-        if dica_usada == True:
-            print("A dica já foi usada!.")
+    indice_group = random.randint(0, 2) #Sorteia entre 0 a 2, posição
+
+    nomes_categorias = list(categorias_animais.keys())
+    tema_sorteado = random.choice(nomes_categorias)
+    choisen_animal = random.choice(categorias_animais[tema_sorteado]).lower()
+
+    lenght_name = len(choisen_animal)
+    hidden_name = [" " if letra == " " else "_" for letra in choisen_animal]
+
+    erros = 0
+    acertos = 0
+    lista_acertos = []
+    lista_erros = []
+    while "_" in hidden_name:
+        limpar()
+
+        if erros == 6:
+            limpar()
+            print(stages[6])
+            derrota = True
+            break
+        print(stages[erros])
+        print(" ".join(hidden_name))
+        print("")
+
+        #Se o usuario usar a dica
+        palpite = input("Palpite: ").lower()
+
+        if palpite == "dica":
+            if dica_usada == True:
+                print("A dica já foi usada!.")
+                time.sleep(1)
+                continue
+            print(f"Dica: {tema_sorteado}")
+            dica_usada = True
             time.sleep(1)
             continue
-        print(f"Dica: {tema_sorteado}")
-        dica_usada = True
-        time.sleep(1)
-        continue
 
-    comprimento_palpite = len(palpite)
-    if comprimento_palpite != 1 and palpite != "dica":
-        print("Só é permitido 1 letra por tentativa, tente novamente.")
-        time.sleep(1)
-        continue
-        
-    elif palpite in choisen_animal:
-        for i in range(len(choisen_animal)):
-            if choisen_animal[i] == palpite:
-                hidden_name[i] = palpite
-        if palpite not in lista_acertos:
-            lista_acertos.append(palpite)
-            acertos += 1
+        #Caso a pessoa escreva mais que 1 letra por palpite
+        comprimento_palpite = len(palpite)
+        if comprimento_palpite != 1 and palpite != "dica":
+            print("Só é permitido 1 letra por tentativa, tente novamente.")
+            time.sleep(1)
+            continue
+
+        if palpite in lista_acertos or palpite in lista_erros:
+            print("Tu já tentaste essa letra, escolha uma diferente!")
+            time.sleep(1)
+            continue
+            
+        elif palpite in choisen_animal:
+            for i in range(len(choisen_animal)):
+                if choisen_animal[i] == palpite:
+                    hidden_name[i] = palpite
+            if palpite not in lista_acertos:
+                lista_acertos.append(palpite)
+                acertos += 1
+        else:
+            erros += 1
+            lista_erros.append(palpite)
+            if erros > 0:
+                print(f"Erros: {erros}")
+            time.sleep(1)
+    if derrota:
+        print("Gamer Over.",end="")
+        time.sleep(0.5)
+        print(".",end="")
+        time.sleep(0.5)
+        print(".",end="")
+        time.sleep(0.5)
+        print(".")
+        print("")
+        print("-= Estatisticas do jogo =-")
+        print(f"Erros: {erros}")
+        print(f"Acertos: {acertos}")
+        print(f"Letras Reveladas/Acertadas: {lista_acertos}")
+        print(f"Letras Erradas: {lista_erros}")
+        print("")
+        print(f"Nome do Animal: {choisen_animal}")
     else:
-        erros += 1
-        lista_erros.append(palpite)
-        time.sleep(1)
-if derrota:
-    print("Gamer Over.",end="")
-    time.sleep(0.5)
-    print(".",end="")
-    time.sleep(0.5)
-    print(".",end="")
-    time.sleep(0.5)
-    print(".")
-    print("")
-    print("-= Estatisticas do jogo =-")
-    print(f"Erros: {erros}")
-    print(f"Acertos: {acertos}")
-    print(f"Letras Reveladas/Acertadas: {lista_acertos}")
-    print(f"Letras Erradas: {lista_erros}")
-    print("")
-    print(f"Nome do Animal: {choisen_animal}")
-else:
-    print("Parabéns, você venceu o jogo.")
-    print("[Estatisticas do jogo]")
-    print(f"Erros: {erros}")
-    print(f"Acertos: {acertos}")
-    print(f"Letras Reveladas/Acertadas: {lista_acertos}")
-    print(f"Letras Erradas: {lista_erros}")
+        print("")
+        print("Parabéns, você venceu o jogo.")
+        print("")
+        print("-= Estatisticas do jogo =-")
+        print(f"Erros: {erros}")
+        print(f"Acertos: {acertos}")
+        print(f"Letras Reveladas/Acertadas: {lista_acertos}")
+        print(f"Letras Erradas: {lista_erros}")
+    saida = input("Gostaria de tentar denovo ?, escreva (sim) para continuar, qualquer outro input o programa será fechado: ")
+    if saida != "sim":
+        continuar = False
+print("Programa encerrando",end="")
+time.sleep(1)
+print(".",end="")
+time.sleep(1)
+print(".",end="")
+time.sleep(1)
+print(".")
+exit()
