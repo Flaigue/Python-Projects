b6d105f7d10d4423feeff2e275c0fa0ab629e4dd
Leandro <leandroreis6282@gmail.com>
2026-02-05 15:14:15 +0000

    Codico Atualizado, falta 2 implementações

--- AimTraining/ProcessaReage.py
+++ AimTraining/ProcessaReage.py
@@ -4,14 +4,25 @@ import os
 
 os.system("cls" if os.name == "nt" else "clear")
 
-def move_button():
-    new_x = random.randint(30, 770)
-    new_y= random.randint(60, 580)
+counter = 0
+
+def action(#event=None):
+    with_window = window.winfo_width()
+    height_window = window.winfo_height()
+    
+    new_x = random.randint(50, with_window - 50)
+    new_y= random.randint(50, height_window - 50)
     mainbutton.place(x=new_x, y=new_y)
+
+def clicking():
+    global counter
+    counter += 1
+    score.config(text=f"Score: {counter}")
+    action()
 
 window = Tk()
 
-window.geometry("800x600") #Tamanho da janela
+window.state("zoomed") #Tamanho da janela
 window.title("Flow Aim") #Nome da Janela
 
 app_icon = PhotoImage(file="C:/Users/Leandro/Documents/Coding/My Own Projects/AimTraining/Python.png") #Variavel do icon
@@ -34,9 +45,19 @@ mainbutton = Button (
     highlightthickness=0,
     padx=15,
     pady=10,
-    command=move_button
+    command=clicking
     )
 mainbutton.place(x=400, y=300)
+
+score = Label(window,
+              text="Score: 0",
+              font=("MS Serif","15"),
+              fg="#ffffff",
+              bg="#000000"
+              )
+score.place(relx=0.99, rely=0.01, anchor=NE )
+
+#window.bind("<Configure>", action)
 
 window.config(background="Black")
 
