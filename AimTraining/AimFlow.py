3f3fc5899226e587e1e76ba969c24c12faaba71a
Leandro <leandroreis6282@gmail.com>
2026-02-09 22:43:56 +0000

    Codico quase finalizado

--- AimTraining/AimFlow.py
+++ AimTraining/AimFlow.py
@@ -15,7 +15,7 @@ def action(event=None):
 
         if with_window > 150 and height_window > 150:
             new_x = random.randint(50, with_window - 50)
-            new_y= random.randint(50, height_window - 50)
+            new_y = random.randint(50, height_window - 50)
             main_button.place(x=new_x, y=new_y)
 
 def clicking():
@@ -34,9 +34,29 @@ def update_timer():
         timer_window.config(text=f"Time: {time}")
         window.after(1000, update_timer)
     else:
-        main_button.config(state=DISABLED)
+        main_button.config(state="disabled")
+        main_button.place_forget()
         main_text.config(text="Game Over !", fg="#FF0000")
+        restart_button.place(relx=0.5, rely=0.60, anchor="center")
+        leave_button.place(relx=0.5, rely=0.75, anchor="center")
 
+def reset_game():
+    global counter, time, game_started
+    counter = 0
+    time = 10
+    game_started = False
+
+    score.config(text="Score: 0")
+    timer_window.config(text="Time: 10sec")
+    main_text.config(text="Train you accuracy here", fg="#ffffff")
+
+    main_button.config(state="normal")
+    main_button.place(relx=0.5, rely=0.5, anchor="center")
+
+    restart_button.place_forget()
+
+def leave():
+    window.destroy()
 
 window = Tk()
 
@@ -56,7 +76,6 @@ main_text.pack()
 
 main_button = Button (
     window,
-    font=("Comic Sans MS", 5),
     background="white",
     relief="flat",
     highlightthickness=0,
@@ -72,7 +91,7 @@ score = Label(window,
               fg="#ffffff",
               bg="#000000"
               )
-score.place(relx=0.99, rely=0.01, anchor=NE)
+score.place(relx=0.99, rely=0.01, anchor="ne")
 
 timer_window = Label(window,
              text="Time: 10sec",
@@ -80,10 +99,33 @@ timer_window = Label(window,
              fg="#ffffff",
              bg="#000000"
              )
-timer_window.place(relx=0.99, rely=0.05,anchor=NE)
+timer_window.place(relx=0.99, rely=0.05,anchor="ne")
+
+restart_button = Button(window,
+                        text = "Restart",
+                        font= ("8514oem", "25"),
+                        padx=5,
+                        pady=3,
+                        bg="#FFFFFF",
+                        fg="#000000",
+                        command=reset_game
+)
+
+leave_button = Button(window,
+                      text= "Leave",
+                      font= ("8514oem", "25"),
+                      padx=3,
+                      pady=3,
+                      bg="#ffffff",
+                      fg="#000000",
+                      command=leave
+)
 
 window.bind("<Configure>", action)
 
 window.config(background="Black")
 
 window.mainloop() #Abre e mantém a janela aberta
+
+#Se é um utilizador comum e chegaste até aqui eu recomendo não mexer no códico, podes quebrar ele, não responsabilizo se você quebrar o codico.
+#Codico foi feito por um amador, pode existir bugs inesperados, você foi avisado.
